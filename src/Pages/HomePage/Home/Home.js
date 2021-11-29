ccda0ae672605710368d8d2b0340d8aa17b187f1
parvezaman <emailtoamanullah@gmail.com>
2021-11-29 09:59:03 +0600

    libraries added

--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h3>This is Home</h3>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga eveniet similique ipsa praesentium, dolores vero eligendi reprehenderit doloremque delectus accusantium optio exercitationem voluptatibus debitis. Rem quasi quidem quibusdam omnis esse, magni soluta dolores molestiae voluptates dicta magnam praesentium asperiores animi sapiente! Minima, sit eos suscipit ab, qui officia nesciunt illum ipsa veniam, esse eum architecto.</p>
         </div>
     );
 };
